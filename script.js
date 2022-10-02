295908cfcb593f4b39f72d360078ade4421f9c45
eva tech <arinzeevaristus640@gmail.com>
2022-10-02 21:33:15 +0100

    initial commit

--- script.js
+++ script.js
@@ -4,3 +4,22 @@ $(document).ready(function () {
         $('.nav-links').slideToggle(3000);
     })
 })
+
+$(window).scroll(function () {
+    let scroll = $(window).scrollTop();
+
+    if (scroll >= 70) {
+        $('#nav').addClass("new-nav");
+    }  else {
+        $('#nav').removeClass("new-nav");
+    }
+})
+
+$('nav a').click(function(link){
+    link.preventDefault();
+
+    let target = $(this).attr('href');
+
+    $('html, body').animate({
+        scrollTop : $(target).offset().top},"slow");
+})
